e6abcb1d126746935c54ec9abe01f0f3c72baf00
kato8088 <kato8088@noreply.users.github.com>
2025-04-07 11:54:41 -0300

    Processo de criação da página de produtos

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,64 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Error from './routes/Error'
 import Home from './routes/Home'
+import Produtos from './routes/Produtos'
 import Login from './routes/Login'
 import Filtro from './routes/Filtro'
 import Jogador from './routes/Jogador'
+import Dados from './Data/Dados.json'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle `
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: #f4f4f4;
+}
+
+#container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+#content-wrap {
+  display: grid;
+  grid-template-column: 1fr 200px;
+  gap: 20px;
+  padding: 20px;
+  flex: 1;
+}
+
+@media (max-width: 768px) {
+  #content-wrap{
+    grid-template-column: 1fr;
+  }
+  aside{
+    display: block;
+    width: 100%;
+  }
+}
+`
 
 function App() {
+
   return (
-  <Router>
-    <Header/>
-    <Routes>
-      <Route path="*" element={<Error/>}/>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/filtro" element={<Filtro/>}/>
-      <Route path="/jogador" element={<Jogador/>}/>
-    </Routes>
-    <Footer/>
-  </Router>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="*" element={<Error/>}/>
+        <Route path="/" element={<Home Dados={Dados}/>}/> 
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/filtro" element={<Filtro/>}/>
+        <Route path="/jogador" element={<Jogador/>}/>      
+      </Routes>
+      <Aside/>
+      <Footer/>
+    </Router>
   )
 }
 
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { ProdutoContainer } from '../styles/ProdutosStyled'
 
-const Produtos = () => {
+const Produtos = ({imagem, titulo, descricao, preco}) => {
   return (
-    <div>
-      
-    </div>
+    <ProdutoContainer>
+    <article className='produto'>
+      <img src={imagem} alt={titulo} />
+      <h3>{titulo}</h3>
+      <p>{descricao}</p>
+      <p className='preco'>{preco}</p>
+      <button>Comprar</button>
+    </article>
+    </ProdutoContainer>
   )
 }
 
